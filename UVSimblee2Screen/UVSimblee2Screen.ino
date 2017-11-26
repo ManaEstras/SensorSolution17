a917fbd097a5c90ac4de016f2f3420d8a7af1470
Mana Yang <jackmanayang@gmail.com>
2017-11-26 10:45:42 -0500

    Update UVSimblee2Screen.ino

--- UVSimblee2Screen/UVSimblee2Screen.ino
+++ UVSimblee2Screen/UVSimblee2Screen.ino
@@ -1,29 +1,6 @@
 
 
-/*
-   Copyright (c) 2015 RF Digital Corp. All Rights Reserved.
 
-   The source code contained in this file and all intellectual property embodied in
-   or covering the source code is the property of RF Digital Corp. or its licensors.
-   Your right to use this source code and intellectual property is non-transferable,
-   non-sub licensable, revocable, and subject to terms and conditions of the
-   SIMBLEE SOFTWARE LICENSE AGREEMENT.
-   http://www.simblee.com/licenses/SimbleeSoftwareLicenseAgreement.txt
-
-   THE SOURCE CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND.
-
-   This heading must NOT be removed from this file.
-*/
-
-/*
-   TwoScreens demonstraits how multiple screens can be used in SimbleeForMobile
-
-   The application consists of two basic screens; each containing a label and a button.
-   When the button on the first screen is pressed, the second screen is displayed
-   and vice versa.
-
-   Button events are also registered for and acted upon in the application.
-*/
 #include <SimbleeNonBLE.h>
 #include <SimbleeForMobile.h>
 #include "YellowDot.h"
